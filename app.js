45f029fe0490ec51d68d5296c141f13c3f49befa
Sowmya Ari <sowmya@Sowmyas-Pro.local>
2019-11-11 23:03:24 +0530

    routes are imported to use by application

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const sequelize = require('./models')
+const cors= require('cors');
+const models = require('./models/index')
+const routes = require('./routes/notes')
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cors())
+app.use(routes)
+
+app.get('/', (req, res) => {
+  res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+});
+
 require('dotenv').config()
 const port = process.env.PORT
-sequelize.sync().then(() => {
+models.sequelize.sync().then(() => {
     app.listen(port, () => {
       console.log(`notes app is listening on port ${port}!`)
     });
