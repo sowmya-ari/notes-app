6846f1fac027befa4f72ac2f686c73f153dab1b0
Sowmya Ari <sowmya@Sowmyas-Pro.local>
2019-11-12 10:08:11 +0530

    added some request validation logics in the file

--- controllers/UserAuthentication.js
+++ controllers/UserAuthentication.js
@@ -1,45 +1,39 @@
 var services = require('../services/UserAuthentication')
 const bcrypt = require('bcrypt');
 
-function validateUser(email,password){
-    const validEmail = typeof email == 'string' && email != '' && email.trim() != '';
-    const validPassword = typeof password == 'string' && password.trim()!='' && password.trim.length>=6 && password != ''
-    return validEmail,validPassword
-}
-
-const  authorizeUser = async function(req,res){
-    const email = req.body.email
+const authorizeUser = async function(req,res){
+    const username = req.body.username
     const password = req.body.password
-    if(validateUser(email,password)){
+    if (!username || !password) {
+        res.status(400).send({msg: 'Please pass username and password.'})
+    }
+    else {
         try{
-            const hashPassword = bcrypt.hashSync(req.body.password, 10);
-            const result = await services.createUser(email,hashPassword)
-            return res.status(201).send('user is created')
+            const hashPassword = bcrypt.hashSync(password,10);
+            const result = await services.authorizeUser(username,hashPassword)
+            return res.status(201).send(result)
         }
         catch (error){
-            return res.status(500).json(error)
+            return res.status(500).send("user already exists ")
         }
-    }
-    else{
-        return res.status(400).send('email or password is invalid')
     }
 }
 
 const authenticateUser = async function(req,res){
-    const email = req.body.email
+    const username = req.body.username
     const password = req.body.password
-    if(validateUser(email,password)){
-        try{
-            const hashPassword = bcrypt.hashSync(req.body.password, 10);
-            const result = await services.authenticatingUser(email,hashPassword)
-            return res.status(201).send('user is valid')
-        }
-        catch (error){
-            return res.status(500).json(error)
-        }
+    if(!username || !password){
+        return res.status(400).send('email or password is invalid')
     }
     else{
-        return res.status(400).send('email or password is invalid')
+        try{
+            const result = await services.authenticateUser(username,password)
+            console.log(result)
+            return res.status(201).send(result)
+        }
+        catch (error){
+            return res.status(500).send(error)
+        }
     }
 }
 module.exports = {
