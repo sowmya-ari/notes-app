4f39d1931a484ccf132c10d9444544050835b3b6
Sowmya Ari <sowmya@Sowmyas-Pro.local>
2019-11-12 06:57:06 +0530

    routes are added in app.js file to use

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors= require('cors');
 const models = require('./models/index')
-const routes = require('./routes/notes')
+const notesRoutes = require('./routes/notes')
+const authenticationRoutes = require('./routes/UserAuthentication')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
-app.use(routes)
+app.use(notesRoutes)
+app.use(authenticationRoutes)
 
 app.get('/', (req, res) => {
   res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
