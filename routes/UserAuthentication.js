881b0a344b9b4b8ad0bc259f597099777de908dd
Sowmya Ari <sowmya@Sowmyas-Pro.local>
2019-11-12 10:27:06 +0530

    file moved to servefr folder

--- routes/UserAuthentication.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var controller = require('../controllers/UserAuthentication')
-
-router.post('/signup',controller.authorizeUser)
-router.post('/signin',controller.authenticateUser)
-module.exports = router;
